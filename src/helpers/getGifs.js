871aa7284e099a8204a2fdc577679da893124a52
Fernando Gomez de Paz <fernando.gomezdepaz@claro.com.ar>
2021-08-20 15:13:54 -0300

    77 -helpers-getGifs

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+ export const getGift = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}}&limit=10&api_key=zfHRdnTexS8UDvn9tVi5qd4Snbq62UXX`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+
+}
