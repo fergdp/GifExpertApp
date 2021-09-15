cb6e729859ec34410c1827dec8bb55027f8c6692
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2021-09-15 19:39:22 -0300

    90 - Pruebas del componente GifGridItem

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const GifGridItem = ({ id, title, url }) => {
-  console.log(id, title, url);
   return (
     <div className="card animate__animated animate__fadeIn">
       <img src={url} alt={title} />
